10b2e210d8c9963df4d49f9ae849c6ab34e0e569
pokpak00 <sorayanilsalai@gmail.com>
2024-01-24 11:44:17 +0700

    XMAS uploed

---
+++ XMAS/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1706071403,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bord-sheet0.png",
+		"images/mask-sheet0.png",
+		"images/mask-sheet1.png",
+		"images/mask-sheet2.png",
+		"images/number.png",
+		"images/optrue-sheet0.png",
+		"images/opfalse-sheet0.png",
+		"images/player-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/hit.ogg",
+		"media/game.ogg",
+		"media/bg.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
